9c4bfbab41e5636e838d0b87edd7b4e4b502a610
pratik-kale20 <ppkale@mitaoe.ac.in>
2021-05-26 15:42:44 +0530

    debug check.py

--- check.py
+++ check.py
@@ -1,4 +1,17 @@
-import subprocess
+import threading
+import os
 
-a=subprocess.check_output("ls | grep log.txt", shell=True)
-print(a)
+def errorCount():
+    threading.Timer(5.0,errorCount).start()
+    a=300
+    out1='abc.txt'
+    if(a>=200):
+        out = os.popen('ls | grep abc.txt').read()
+        print(out)
+        b=(out1==out)
+        print(b)
+        if(b==True):          
+            os.system('mv abc.txt xyz.py')
+            os.system('service nginx reload')
+            print("File successfully renamed")	
+errorCount()
--- log.py
+++ log.py
@@ -2,14 +2,15 @@ import threading
 
 def errorCount():
     threading.Timer(5.0,errorCount).start()
-    fname = ("/var/log/nginx/access.log")
+    fname = ("log.txt")
     num1="200"
     num2="499"
     num3="500"
     count1= 0;
     count2= 0;
     count3= 0;
-
+    sum=0;
+    out1='abc.txt'
     with open(fname, 'r') as f:
         for line in f:
             words1= line.split()
@@ -31,6 +32,15 @@ def errorCount():
     print("Occurrences of the number",num1,"is",count1)
     print("Occurrences of the number",num2,"is",count2)
     print("Occurrences of the number",num3,"is",count3)
-    print("Total Erros:",count2+count3)
-
+    sum=count2+count3
+    print("Total Erros:",sum)
+        
+     if(sum>=200):
+        out = os.popen('ls | grep abc.txt').read()
+        print(out)
+        b=(out1==out)
+        print(b)
+        if(b==True):
+            os.system('mv abc.txt xyz.py')
+            print("File successfully renamed")
 errorCount()
